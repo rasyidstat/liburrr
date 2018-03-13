fad1a2c8e5abfc9c39fa8ee555992c4818969eca
Rasyid Ridha <rasyidstat@gmail.com>
2018-03-13 21:11:47 +0700

    add dataset

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' @importFrom tibble tibble
+NULL
+
+#' Public Holiday in Indonesia
+#'
+#' List of public holiday in Indonesia from 2013 to 2018
+#'
+#' @docType data
+#'
+#' @md
+#' @format A tibble containing list of public holiday in Indonesia with date column
+#'
+#' @source \href{http://www.liburnasional.com/}{http://www.liburnasional.com/}
+#'
+"libur"
